a1e912e76697785ebe2a392e49d62b51c7870c85
feliciebernad <felicie.bernard@outlook.fr>
2021-12-01 21:54:43 +0100

    pictures added

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   get '/la-maison' => 'static_pages#maison'
   get '/le-chalet' => 'static_pages#chalet'
   get '/le-port' => 'static_pages#port'
-  get '/les-alentours' => 'static_pages#alentours'
   get '/la-dépendance' => 'static_pages#dependance'
-  get '/le-parc-résidentiel' => 'static_pages#parcresidentiel'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "home_page#home"
