ea3bfa87c3e1acc409d0c24ec163866bece9f272
feliciebernard <felicie.bernard@outlook.fr>
2022-01-13 09:38:48 +0100

    image modules activate

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,5 @@ class Picture < ApplicationRecord
   belongs_to :place, optional: true
   has_one_attached :image
 
-  validates :image, presence: true, blob: { content_type: :image, size_range: 5..(400.kilobytes) }
+  validates :image, presence: true, blob: { content_type: :image, size_range: 5..(500.kilobytes) }
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,5 +2,5 @@ class Place < ApplicationRecord
   has_many :pictures
   has_one_attached :main_picture
 
-    validates :main_picture, presence: true, blob: { content_type: :image, size_range: 5..(400.kilobytes) }
+    validates :main_picture, presence: true, blob: { content_type: :image, size_range: 5..(500.kilobytes) }
 end
